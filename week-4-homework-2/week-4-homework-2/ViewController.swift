b0a50edddf9aaf56f731c681f917cbcf44298a93
kamilcal <kamilcaal@gmail.com>
2023-01-04 02:08:22 +0300

    project file has been created

---
+++ week-4-homework-2/week-4-homework-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-4-homework-2
+//
+//  Created by kamilcal on 4.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
