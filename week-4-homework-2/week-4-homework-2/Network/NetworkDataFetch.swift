e68b29908a8aa0d224bf649a7eb0172a7f4336ec
kamilcal <kamilcaal@gmail.com>
2023-01-04 13:46:59 +0300

    network layer structure

--- week-4-homework-2/week-4-homework-2/Network/NetworkDataFetch.swift
+++ week-4-homework-2/week-4-homework-2/Network/NetworkDataFetch.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class NetworkDataFetch {
+    static let shared = NetworkDataFetch()
+    let decoder = JSONDecoder()
+    
+    func fetchAlbum(urlString: String, response: (AlbumModel?, Error?) -> Void) {
+        NetworkRequest.shared.request(urlString: urlString) { result in
+            switch result {
+                
+            case .success(let data):
+                do {
+                    let jsonAlbum = try decoder.decode(AlbumModel.self, from: data)
+                    response(jsonAlbum, nil)
+                } catch let jsonError {
+                    print("Failed to decode json \(jsonError.localizedDescription)")
+                }
+            case .failure(let error):
+                print("Error request: \(error.localizedDescription)")
+                response(nil,error)
+            }
+        }
+    }
+    
+    func fetchSong(urlString: String, response: (SongsModel?, Error?) -> Void) {
+        NetworkRequest.shared.request(urlString: urlString) { result in
+            switch result {
+                
+            case .success(let data):
+                do {
+                    let jsonSong = try decoder.decode(SongsModel.self, from: data)
+                    response(jsonSong, nil)
+                } catch let jsonError {
+                    print("Failed to decode json \(jsonError.localizedDescription)")
+                }
+            case .failure(let error):
+                print("Error request: \(error.localizedDescription)")
+                response(nil,error)
+            }
+        }
+    }
+}
--- week-4-homework-2/week-4-homework-2/Network/NetworkRequest.swift
+++ week-4-homework-2/week-4-homework-2/Network/NetworkRequest.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class NetworkRequest {
+    static let shared = NetworkRequest()
+    
+    
+    func request(urlString: String, completion: (Result<Data, Error>) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if let error = error{
+                    completion(.failure(error))
+                    return
+                }
+                guard let data = data else { return }
+                completion(.success(data))
+            }
+        }
+        .resume()
+    }
+}
+
